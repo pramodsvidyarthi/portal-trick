1d6f8e17e863500f06fd64ab6dc773eaabfcefff
pramod <pramodsvidyarthi@gmail.com>
2020-04-08 12:15:00 +0530

    portal component changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Portal from "./Portal";
 import Tabs from "./Tabs";
 import "./styles.css";
 
-const One = ({ portalRef }) => {
-  const [value, setValue] = useState();
+const Search = ({ onChange, counter }) => {
+  const [value, setValue] = useState("");
+  const handleChange = useCallback(
+    ({ target: { value } }) => {
+      setValue(value);
+      onChange(value);
+    },
+    [setValue, onChange]
+  );
   return (
     <>
-      <Portal mountNode={portalRef}>
-        <h2>Start typing to see the magic happen!</h2>
-        <input
-          value={value}
-          onChange={({ target: { value } }) => setValue(value)}
-          className="m-b-1"
-        />
-      </Portal>
+      <input value={value} onChange={handleChange} className="m-b-1" />
+      <h1>input value inside portal {value}</h1>
+      <h1>counter value inside portal {counter}</h1>
+    </>
+  );
+};
+
+Search.defaultProps = {
+  onChange: f => f
+};
+
+const One = ({ portalRef }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [counter, setCounter] = useState(0);
+  const handleIncrement = useCallback(() => {
+    setCounter(currentValue => currentValue + 1);
+  }, []);
+  const handleDecrement = useCallback(() => {
+    setCounter(currentValue => currentValue - 1);
+  }, []);
+  return (
+    <>
+      {portalRef && (
+        <Portal mountNode={portalRef}>
+          <h2>Start typing to see the magic happen!</h2>
+          <Search counter={counter} onChange={setInputValue} />
+        </Portal>
+      )}
       <h1>one</h1>
       <div>one is active</div>
-      {value}
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <h1>input value outside portal {inputValue}</h1>
+      <h1>counter value outside portal {counter}</h1>
     </>
   );
 };
@@ -36,15 +66,15 @@ const Header = React.forwardRef((props, ref) => (
 ));
 
 export default function App() {
-  const [portalRef, setRef] = useState();
+  const [ref, setRef] = useState();
   return (
     <div className="App">
       <Header ref={setRef} />
-      <Tabs initllActive={2}>
+      <Tabs initllActive={1}>
         <Tabs.Tab value={1}>one</Tabs.Tab>
         <Tabs.Tab value={2}>two</Tabs.Tab>
         <Tabs.Content when={1}>
-          <One portalRef={portalRef} />
+          <One portalRef={ref} />
         </Tabs.Content>
         <Tabs.Content when={2}>
           <Two />
